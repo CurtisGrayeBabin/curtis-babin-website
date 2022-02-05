610d1295cbc17e3c1732fc5299622ceaabb88aa1
CurtisGrayeBabin <curtsocal@gmail.com>
2022-02-05 13:42:19 -0800

    Add About section + alter layout padding + global link hover color changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Layout from "../components/layout";
-import { increase, contact, border } from "./index.module.css";
+import { increase, contact, border, about } from "./index.module.css";
 import { layoutPadding } from "../global styles/layout.module.css";
 
 const IndexPage = () => {
@@ -11,6 +11,24 @@ const IndexPage = () => {
           Grow your business with a fast, mobile, modern, & SEO-focussed website
         </h1>
         <h2 className={contact}>Contact me below for more details</h2>
+      </section>
+      <section className={`${layoutPadding}`}>
+        <div className={about}>
+          <h3>About</h3>
+          <p>
+            I am a professional web developer with over{" "}
+            {new Date().getFullYear() - 2017} years of experience in HTML, CSS,
+            and JavaScript. My Bachelor of Science in Computer Science is from
+            UC Irvine. In addition I hold a Full Stack JavaScript Developer
+            Nanodegree certification through Udacity. My skills also include
+            React, Responsive Design, and UI Design. So thank you for stopping
+            by! Catch me being active on{" "}
+            <a href="https://www.linkedin.com/in/curtisbabin/" target="_">
+              LinkedIn
+            </a>
+            .
+          </p>
+        </div>
       </section>
     </Layout>
   );
