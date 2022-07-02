928ba4d2c006dcc34f5544962887d8f7079c6088
Curtis Babin <curtsocal@gmail.com>
2022-07-02 13:05:05 -0700

    Add profile image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Curtis Babin | Web Developer">
 
-
+    <div>
+      <StaticImage src="../images/github profile.png" alt="My picture" width={300} height={300} imgStyle={{ borderRadius: '100%' }} />
+    </div>
 
     </Layout>
   );
