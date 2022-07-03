8711e8c044a23b118f4dc3b650c072fc1c091d71
Curtis Babin <curtsocal@gmail.com>
2022-07-03 14:25:42 -0700

    Add links around center pic

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Layout from "../components/Layout";
 import { StaticImage } from "gatsby-plugin-image";
+import "../styles/index.css";
 
 const IndexPage = () => {
 
@@ -8,10 +9,42 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Curtis Babin | Web Developer">
     
+    
+
     {/* profile pic container */}
-    <div style={{"display": "flex", "justifyContent": "center", "alignItems": "center", "height": "calc(75vh)"}}>
-        <StaticImage src="../images/github profile.png" alt="My picture" width={300} height={300} imgStyle={{ borderRadius: '100%' }} />
+    <div style={{"display": "flex", "justifyContent": "center", "alignItems": "center", "height": "85vh"}}>
+      
+      <div>
+        <a href="/" className="link">About</a>&nbsp;
+      </div>
+
+      <div>
+        <table>
+          <tr>
+            <td align="center">
+              <a href="https://github.com/CurtisGrayeBabin" target="_" className="link">GitHub</a>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <StaticImage src="../images/github profile.png" alt="My picture" width={300} height={300} imgStyle={{ borderRadius: '100%' }} />
+            </td>
+          </tr>
+          <tr>
+            <td align="center">
+              <a href="https://www.linkedin.com/in/curtisbabin/" target="_" className="link">LinkedIn</a>
+            </td>
+          </tr>
+        </table>
+      </div>
+
+      <div>
+      &nbsp;<a href="/" className="link">Works</a>
+      </div>
+
     </div>
+
+    
 
     </Layout>
   );
