052ebc949ed338be1b11021aacb1441977d3ffb7
Curtis Babin <curtsocal@gmail.com>
2022-09-03 12:40:27 -0700

    Add border to Udacity image and clean up about me section text

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,16 +15,16 @@ const IndexPage = () => {
     <h1 className="heading">About</h1>
 
     <p className="contentText">
-    I am a programmer with over four years of professional experience. 
-    I enjoy creating websites and helping others realize their goals with code! 
-    I graduated from the University of California, Irvine, and earned my Bachelor of Science degree in Computer Science. 
+    I am a freelance developer; I have been a professional developer since 2017. 
+    I enjoy creating websites, applications, and whatever helps others realize their goals! 
+    My alma mater is the University of California, Irvine, where I earned my Bachelor of Science degree in Computer Science. 
     I have also completed two bootcamps concerning website development with a specialization in Node.js.
-    Feel free to email me regarding inquiries: curtis [at] curtisbabin [dot] com
+    Please feel free to email me regarding inquiries: curtis [at] curtisbabin [dot] com
     </p>
 
     <div style={{"display": "flex", "justifyContent": "space-evenly", "alignItems": "center", "flexWrap": "wrap", "paddingTop": "1rem"}}>
-        <StaticImage src="../images/udacity.jpg" alt="My Udacity Nanodegree image" width={400} height={300} />
         <StaticImage src="../images/codecademycert.jpg" alt="My CodeCademy Web Developer certification image" width={400} height={300} />
+        <StaticImage src="../images/udacity.jpg" alt="My Udacity Nanodegree image" width={400} height={300} style={{border: "1px solid black"}} />
     </div>
 
     </PageLayout>
