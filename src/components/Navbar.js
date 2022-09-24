0b0b5007bc569770a87e6e137eb4b7a2a3446dd5
Curtis Babin <curtsocal@gmail.com>
2022-09-24 15:09:10 -0700

    Place navbar SVG in div to maintain dimensions on small-width screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,20 +18,22 @@ function Navbar() {
     <Headroom>
       <nav className="nav layout-navbar">
         <Link to="/" className="link-H1">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="svg1"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
-            />
-          </svg>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="svg1"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
+              />
+            </svg>
+          </div>
           &nbsp;
           <h1>Curtis Babin <span style={{"display":"inline-block"}}>| Web Developer</span></h1>
         </Link>
