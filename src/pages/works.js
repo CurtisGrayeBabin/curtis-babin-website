cb40b31527f2e3f304ce4e273dbfb73d1630ac24
Curtis Babin <curtsocal@gmail.com>
2022-10-01 13:26:46 -0700

    Put Works items into a flex container rather than keep them separated

--- src/pages/works.js
+++ src/pages/works.js
@@ -16,43 +16,44 @@ const IndexPage = () => {
     
     <h1 className="heading">Works</h1>
 
-    <div className="project-container">
-        <h2><a href="http://steviesdetails.com" target="_" className="project-title">Stevie's Details&nbsp;<BiLinkExternal /></a></h2>
-        
-        <div className="project-image">
-            <StaticImage src="../images/steviesDetails.png" alt="Screenshot of the Stevie's Details homepage" height={400} />
+    <div className="projects-container">
+        <div className="project-container">
+            <h2><a href="http://steviesdetails.com" target="_" className="project-title">Stevie's Details&nbsp;<BiLinkExternal /></a></h2>
+            
+            <div className="project-image">
+                <StaticImage src="../images/steviesDetails.png" alt="Screenshot of the Stevie's Details homepage" height={400} />
+            </div>
+
+            <ul className="project-contents">
+                <li>
+                Orange County business's website built on Gatsby (React), HTML, CSS, JSX
+                </li>
+                <li>
+                Responsive design with custom styling
+                </li>
+            </ul>
         </div>
 
-        <ul className="project-contents">
-            <li>
-            Orange County business's website built on Gatsby (React), HTML, CSS, JSX
-            </li>
-            <li>
-            Responsive design with custom styling
-            </li>
-        </ul>
-    </div>
+        <div className="project-container">
+            <h2><a href="http://store.curtisbabin.com" target="_" className="project-title">My Store&nbsp;<BiLinkExternal /></a></h2>
+            
+            <div className="project-image">
+                <StaticImage src="../images/store.png" alt="Screenshot of my store application" width={500} height={400} />
+            </div>
 
-    <div className="project-container">
-        <h2><a href="http://store.curtisbabin.com" target="_" className="project-title">My Store&nbsp;<BiLinkExternal /></a></h2>
-        
-        <div className="project-image">
-            <StaticImage src="../images/store.png" alt="Screenshot of my store application" width={500} height={400} />
+            <ul className="project-contents">
+                <li>
+                Angular e-commerce application
+                </li>
+                <li>
+                Uses the Web Storage API (localStorage) for a persisting cart across sessions
+                </li>
+                <li>
+                Responsive design with Bootstrap styling
+                </li>
+            </ul>
         </div>
-
-        <ul className="project-contents">
-            <li>
-            Angular e-commerce application
-            </li>
-            <li>
-            Uses the Web Storage API (localStorage) for a persisting cart across sessions
-            </li>
-            <li>
-            Responsive design with Bootstrap styling
-            </li>
-        </ul>
     </div>
-
 
 
     </PageLayout>
