0ebbec8d1f36643b9071f2dd71c79ed3b72cbbdc
Curtis Babin <curtsocal@gmail.com>
2022-10-15 12:44:24 -0700

    Make 404 page simpler + remove 404 CSS file

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,12 @@
 import * as React from "react";
 import PageLayout from "../components/PageLayout";
 import { Link } from "gatsby";
-import "../styles/layout.css";
-import "../styles/404.css";
 
 const NotFoundPage = () => {
   return (
-    <PageLayout pageTitle="404 Error">
-      <h1 className="error">404 Error</h1>
-      <h2>Page not found.</h2>
-      <Link to="/" className="link link-404">
-        Return home
+    <PageLayout pageTitle="404 | Page Not Found">
+      <Link to="/" className="link transition-timing">
+        <h1>404 Error: Page not found! Click this text to return home.</h1>
       </Link>
     </PageLayout>
   );
