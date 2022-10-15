26a7cd689a8fa6397b5932e4619c86578f27c03c
Curtis Babin <curtsocal@gmail.com>
2022-10-15 10:41:29 -0700

    Add box shadow to side menu + remove side menu css file

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "gatsby";
 import { BiLinkExternal } from "react-icons/bi";
-import "../styles/sideMenu.css";
+import "../styles/navbar.css";
 
 
 function SideMenu(props) {
 
   return props.open ? (
     
-      <nav aria-label="Side Menu Navigation" className="side-menu-container">
+      <nav aria-label="Side Menu Navigation" className="side-menu-container bottom-shadow">
           <ul>
               <li><Link to="/" className="side-menu-link">Home&nbsp;<BiLinkExternal /></Link></li>
               <li><Link to="/About" className="side-menu-link">About&nbsp;<BiLinkExternal /></Link></li>
--- src/pages/index.js
+++ src/pages/index.js
@@ -41,7 +41,7 @@ const IndexPage = () => {
 
   
     <div className="click-me-div">
-      <button id="click-me-button" onClick={buttonClicked} className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+      <button id="click-me-button" onClick={buttonClicked} className="bg-transparent hover:border-transparent hover:bg-blue-500 hover:text-white text-blue-700 font-semibold py-2 px-4 border border-blue-500 rounded">
         +1
       </button>
 
