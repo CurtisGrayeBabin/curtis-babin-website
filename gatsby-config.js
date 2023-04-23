f86f7baddbae93e9cc7de0075fe825ecfd4c80be
Curtis Babin <curtsocal@gmail.com>
2023-04-23 15:33:44 -0700

    Begin rewrite

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Curtis Babin`,
+    description: `The personal website of Curtis Babin (a web developer)`,
+    author: `Curtis Babin`,
+    siteUrl: `https://www.curtisbabin.com`,
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -28,15 +28,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Curtis Babin | Web Developer`,
+        short_name: `Curtis Babin`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#1A1152`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        theme_color: `#1A1152`,
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
   ],
